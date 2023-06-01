5b2073bdb4cf211cf8f2f13af5128dd8b9eb1b47
Xethrus <xethrus@gmail.com>
2023-05-31 20:23:16 -0400

    remove fluff

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use cursive::traits::*;
 use cursive::views::{Dialog, DummyView, LinearLayout, TextView};
 
 use std::fs::read_dir;
-use std::ffi::OsString;
-use anyhow::{anyhow, Result};
+use anyhow::{Result};
 
 fn get_current_dir() -> Result<Vec<std::fs::DirEntry>> {
     let mut current_dir_contents: Vec<std::fs::DirEntry> = Vec::new();
@@ -15,7 +14,7 @@ fn get_current_dir() -> Result<Vec<std::fs::DirEntry>> {
     Ok(current_dir_contents)
 }
 
-fn get_file_name(index: usize, current_dir: Vec<std::fs::DirEntry>) -> Result<String> {
+fn get_file_name(index: usize) -> Result<Stri{
     let current_dir_entries = get_current_dir()?;
     let entry = current_dir_entries.get(index).ok_or(anyhow::anyhow!("index out of bounds"))?;
     let file_name = entry.file_name();
@@ -26,7 +25,6 @@ fn get_file_name(index: usize, current_dir: Vec<std::fs::DirEntry>) -> Result<St
 fn display_current_dir() -> Result<()> {
     //let mut current_dir: Vec<std::fs::DirEntry> = Vec::new();
     let mut siv = cursive::default();
-    let mut current_dir = get_current_dir();
     let mut current_index = 0;
 
     siv.add_layer(
@@ -34,7 +32,7 @@ fn display_current_dir() -> Result<()> {
            LinearLayout::vertical()
             .child(TextView::new("view").h_align(HAlign::Center))
             .child(DummyView.fixed_height(1))
-            .child(TextView::new(get_file_name(current_index, get_current_dir()?)?))
+            .child(TextView::new(get_file_name(current_index)?))
             .fixed_width(30),
         )
         .button("exit", |s| s.quit())
@@ -42,11 +40,6 @@ fn display_current_dir() -> Result<()> {
     );
     siv.run();
     Ok(())
-}
-
-//debug
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
 }
 
 fn main() {
